ac9026635e9fdbd9b24a029a259480f3d83bbbd3
saarthack <ssarthak2003@outlook.com>
2024-07-22 17:22:35 +0530

    GSAP - topic: gsap timeline

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-gsap.from("h1",{
-    x:-500,
-    duration:1,
-    delay:0.5,
-    opacity:0,
-    stagger:0.3
+var tl = gsap.timeline()
+
+tl.to("#box1",{
+    x:1000,
+    duration:1.5,
+    delay:0.5
+})
+tl.to("#box2",{
+    x:1000,
+    duration:1.75,
+    backgroundColor:"yellow",
+    rotate:360
+})
+tl.to("#box3",{
+    duration:1.25,
+    x:1000,
+    backgroundColor:"white",
+    scale:0.5,
+    borderRadius:"50%"
 })
