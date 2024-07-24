661967b5b76888a30af0ba3d6a3bfdbb0d385cad
saarthack <ssarthak2003@outlook.com>
2024-07-24 17:42:44 +0530

    GSAP - topic: ScrollTrigger2

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-gsap.from("#page1 #box",{
-    scale:0,
-    duration:1.5,
-    delay:0.5,
-    rotate:360
-})
-gsap.from("#page2 #box",{
-    scale:0,
-    rotate:360,
-    duration:1.5,
+gsap.from(".card",{
+    y:100,
+    opacity:0,
+    duration:0.8,
+    stagger:0.3,
     scrollTrigger:{
-        trigger:"#page2 #box",
+        trigger:".card",
         scroller:"body",
-        markers:true,
         start:"top 50%",
-        end:"top 30%",
+        end:"top 40%",
         scrub:3
     }
 })
