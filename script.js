f3133e7312dbb086438af219c139c8251528ed1c
saarthack <ssarthak2003@outlook.com>
2024-07-24 14:21:37 +0530

    GSAP - topic: ScrollTrigger

--- script.js
+++ script.js
@@ -1,53 +1,19 @@
-var tl = gsap.timeline()
-
-tl.from("nav h2",{
-    y:-60,
-    duration:0.4,
-    delay:0.4,
-    opacity:0
+gsap.from("#page1 #box",{
+    scale:0,
+    duration:1.5,
+    delay:0.5,
+    rotate:360
 })
-tl.from("nav h4",{
-    y:-60,
-    opacity:0,
-    duration:0.6,
-    stagger:0.2
-})
-
-tl.from(".center h1",{
-    y:80,
-    opacity:0,
-    duration:0.4
-})
-tl.from("#img3,#img4",{
-    opacity:0,
-    scale:1.5,
-    duration:0.4
-})
-tl.from(".center p",{
-    y:80,
-    opacity:0,
-    duration:0.3
-})
-tl.from(".center button",{
-    scale:1.4,
-    opacity:0,
-    duration:0.3
-})
-
-tl.from("#img1",{
-    x:-300,
-    duration:0.4,
-    opacity:0
-})
-tl.from("#img2",{
-    x:300,
-    duration:0.4,
-    opacity:0
-},'-=0.4')
-
-tl.to("#img3,#img4",{
-    scale:1.1,
-    duration:4,
-    repeat:-1,
-    yoyo:true
+gsap.from("#page2 #box",{
+    scale:0,
+    rotate:360,
+    duration:1.5,
+    scrollTrigger:{
+        trigger:"#page2 #box",
+        scroller:"body",
+        markers:true,
+        start:"top 50%",
+        end:"top 30%",
+        scrub:3
+    }
 })
