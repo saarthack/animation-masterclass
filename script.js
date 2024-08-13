73728a8862696b77d1cf6d6a0c6e07175118187b
saarthack <ssarthak2003@outlook.com>
2024-08-13 14:32:10 +0530

    Locomotive JS Basics

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-var body = document.querySelector("body")
-var h1 = document.querySelector("h1")
-
-body.addEventListener("mousemove",function(dets){
-  gsap.to("#cursor",{
-    x:dets.x,
-    y:dets.y
-  })
-})
-
-h1.addEventListener("mouseenter",function(){
-  gsap.to("#cursor",{
-    scale:5
-  })
-})
-
-h1.addEventListener("mouseleave",function(){
-  gsap.to("#cursor",{
-    scale:1
-  })
-})
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('#main'),
+  smooth: true
+});
