0e02e95f1c69c2a01419d395060ca783faa4678a
saarthack <ssarthak2003@outlook.com>
2024-08-13 12:15:25 +0530

    gsap practice-1/2

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-gsap.from(".card",{
-    y:100,
-    opacity:0,
-    duration:0.8,
-    stagger:0.3,
-    scrollTrigger:{
-        trigger:".card",
-        scroller:"body",
-        start:"top 50%",
-        end:"top 40%",
-        scrub:3
-    }
+var h1Text = document.querySelector("h1").textContent
+
+var splittedText = h1Text.split("")
+
+var clutter = ""
+
+splittedText.forEach(function (elem, idx) {
+  if (splittedText.length / 2 > idx) {
+    clutter += `<span class="a">${elem}</span>`
+  } else {
+    clutter += `<span class="b">${elem}</span>`
+  }
+  
+})
+
+
+document.querySelector("h1").innerHTML = clutter
+
+
+gsap.from("h1 .a", {
+  opacity: 0,
+  duration: 0.6,
+  stagger: 0.2,
+  delay: 0.4,
+  y: 60
+})
+gsap.from("h1 .b", {
+  opacity: 0,
+  duration: 0.6,
+  stagger: -0.2,
+  delay: 0.4,
+  y: 60
 })
