a983552b65813c8cba251d93c56c41ada644f3cb
saarthack <ssarthak2003@outlook.com>
2024-08-16 13:02:38 +0530

    Swiper.js Advance-02

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-var swiper = new Swiper('.swiper',{
-  navigation:{
-    nextEl:"#next-btn",
-    prevEl:"#prev-btn",
+var swiper = new Swiper('.swiper', {
+  navigation: {
+    nextEl: "#next-btn",
+    prevEl: "#prev-btn",
   },
-  pagination:{
-    el:'.swiper-pagination',
+  // rewind: true,
+  loop:true,
+  autoplay:{
+    delay:2500,
+    disableOnInteraction:true
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:true,
     // dynamicBullets:true,
     // type:'progressbar',
     // type:'fraction'
-  }
+  },
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  //   hide:true 
+  // },
 })
